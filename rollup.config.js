6c26dabf318bea86ca0aa281aec22a62cd1e879f
Richard Adjogah <richard@webs.com>
2021-10-05 19:00:17 -0400

    Add electron-forge

--- rollup.config.js
+++ rollup.config.js
@@ -7,27 +7,6 @@ import preprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
-function serve() {
-	// let server;
-
-	function toExit() {
-		// if (server) server.kill(0);
-	}
-
-	return {
-		writeBundle() {
-			// if (server) return;
-			// server = require('child_process').spawn('npm', ['run', 'svelte:dev', '--', '--dev'], {
-			// 	stdio: ['ignore', 'inherit', 'inherit'],
-			// 	shell: true
-			// });
-
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
-}
-
 export default {
 	input: 'src/index.js',
 	output: {
@@ -66,10 +45,6 @@ export default {
 			dedupe: ['svelte']
 		}),
 		commonjs(),
-
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
